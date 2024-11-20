76e2b240fd62dfbad66edc027497efd47ef25ef8
Robert-Penrod <robertpenrod42@gmail.com>
2024-11-20 14:39:03 -0500

    Clean up genre code

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -6,6 +6,7 @@ import {
     Text,
     Spinner,
     Button,
+    Heading,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
@@ -22,45 +23,54 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 
     if (error) return null;
 
-    if (isLoading)
-        return (
-            <List>
-                {skeletons.map((x) => {
-                    return (
-                        <ListItem key={x} paddingY='5px'>
-                            <GenreSekelton />
-                        </ListItem>
-                    );
-                })}
-            </List>
-        );
-
     return (
-        <List>
-            {data.map((genre) => (
-                <ListItem key={genre.id} paddingY='5px'>
-                    <HStack>
-                        <Image
-                            boxSize='32px'
-                            borderRadius={8}
-                            src={getCroppedImageUrl(genre.image_background)}
-                        />
-                        <Button
-                            fontWeight={
-                                genre.id === selectedGenre?.id
-                                    ? "bold"
-                                    : "normal"
-                            }
-                            onClick={() => onSelectGenre(genre)}
-                            fontSize='lg'
-                            variant='link'
-                        >
-                            {genre.name}
-                        </Button>
-                    </HStack>
-                </ListItem>
-            ))}
-        </List>
+        <>
+            <Heading fontSize='2xl' marginBottom={3}>
+                Genres
+            </Heading>
+            {isLoading ? (
+                <List>
+                    {skeletons.map((x) => {
+                        return (
+                            <ListItem key={x} paddingY='5px'>
+                                <GenreSekelton />
+                            </ListItem>
+                        );
+                    })}
+                </List>
+            ) : (
+                <List>
+                    {data.map((genre) => (
+                        <ListItem key={genre.id} paddingY='5px'>
+                            <HStack>
+                                <Image
+                                    objectFit='cover'
+                                    boxSize='32px'
+                                    borderRadius={8}
+                                    src={getCroppedImageUrl(
+                                        genre.image_background
+                                    )}
+                                />
+                                <Button
+                                    whiteSpace='normal'
+                                    textAlign='left'
+                                    fontWeight={
+                                        genre.id === selectedGenre?.id
+                                            ? "bold"
+                                            : "normal"
+                                    }
+                                    onClick={() => onSelectGenre(genre)}
+                                    fontSize='lg'
+                                    variant='link'
+                                >
+                                    {genre.name}
+                                </Button>
+                            </HStack>
+                        </ListItem>
+                    ))}
+                </List>
+            )}
+        </>
     );
 };
 
